30fc962ab43de1db8872c7e4a5e7b048263a100f
wOL-Lucas <lucasefernandes333@gmail.com>
2023-12-21 14:44:54 -0300

    implementing:
    web socket connection and messages.
    Now execution functions received by WSockets, that are received by the server.
    Include: query, scroll and click functions

--- extension/content.js
+++ extension/content.js
@@ -1,45 +1,140 @@
-console.log(":::web-automation-core inited:::");
+(() =>{
+    console.log(":::web-automation-core inited:::");
 
-var socket = new WebSocket('wss://localhost:6901');
+    const socket = new WebSocket('wss://localhost:6901');
 
-socket.onopen = function(e){
-    socket.send('newTab', {id: generateId(), title: document.title, url: document.URL, context: "chrome-web-extension", type: "chrome"});
-};
-
-socket.onmessage = (e) =>{
-    console.log(e);
-};
-
-socket.onerror = function(e){
-    console.log("error ",e);
-};
-
-socket.onclose = function(e){
-    socket.emit('closeTab', {id: generateId(), title: document.title, url: document.URL, context: "chrome-web-extension", type: "chrome"});
-};
-
-
-const generateId = () => {
-    const request = new XMLHttpRequest();
-    request.open('GET', 'https://localhost:6901/api/sessions', false);
-    request.onreadystatechange = () => {
-        if (request.status == 200){
-            var sessions = JSON.parse(request.responseText).sessions;
-
-            var id = document.title;
-            for(var i; i < sessions.length; i++){
-                console.log(sessions[i].id)
-                if(sessions[i].id == id){
-                    id = id + Math.floor(Math.random() * 1000);
-                }
+    const functions = {
+        clickOnButton: (className, index=null) =>{
+            if (index){
+                const button = document.getElementsByClassName(className)[index];
+                button.click();
+                return;
             }
-        
-            return id
-        }
-        else{
-            console.log(":::[web-automation-core] : Could not generate session id :::");
-            throw new Error(":::[web-automation-core] : Could not generate session id :::");
-        }
+
+            const button = document.getElementsByClassName(className)[0];
+            button.click();
+        },
+
+        getElementInnerHtml: (className,index) =>{
+            if (index){
+                const element = document.getElementsByClassName(className)[index];
+                return element.innerHTML;
+            }
+
+            const element = document.getElementsByClassName(className)[0];
+            if(element){
+                return element.innerHTML;
+            }
+            
+            return 'undefined'
+        },
+
+        getElementInnerText: (className, index) =>{
+            if (index){
+                const element = document.getElementsByClassName(className)[index];
+                return element.innerText;
+            }
+
+            const element = document.getElementsByClassName(className)[0];
+            return element.innerText;
+        },
+
+        scrollDown: (to=null) =>{
+            if (to){
+                window.scrollTo(0,to);
+                return;
+            }
+            window.scrollTo(0,document.body.scrollHeight);
+            return;
+        },
+
+        scrollUp: (to=null) =>{
+            if (to){
+                window.scrollTo(0,to);
+                return;
+            }
+            window.scrollTo(0,0);
+            return;
+        },
+
+        findElementByClassName: (className, index=null) =>{
+            if (index){
+                return document.getElementsByClassName(className)[index];
+            }
+
+            return document.getElementsByClassName(className)[0];
+        },
+
+        consoleLog: (message) =>{
+            console.log(message);
+        },
+
+        getElementByFrameworkAttribute: (attribute, value, index=null) =>{
+            const elements = document.querySelectorAll(`\\[${attribute}="${value}"]`);
+            if (index){
+                return elements[index];
+            }
+            return elements[0];
+        },
     }
-    request.send();
-}
+    
+    socket.onopen = function(e){
+        console.log("opened")
+        socket.send(JSON.stringify(
+            {
+                "type":"register",
+                "url": document.URL,
+                "title": document.title,
+                "timestamp": Date.now(),
+            }
+        ));
+    };
+    
+    socket.onmessage = (e) =>{
+        const data = JSON.parse(e.data);
+        switch(data.type){
+            case "greet":
+                console.log("greet");
+                break;
+    
+            case "execute":
+                console.log("execute", data.execute)
+                if(functions[data.execute.function]){
+                    const result = functions[data.execute.function](...data.execute.args);
+                    socket.send(JSON.stringify(
+                        {
+                            "type":"executeResult",
+                            "result": result,
+                            "id": data.id
+                        }
+                    ));
+                }
+                else{
+                    console.log("function not found");
+                    socket.send(JSON.stringify(
+                        {
+                            "type":"executeResult",
+                            "result": null,
+                            "id": data.id
+                        }
+                    ));
+                }
+                break;
+    
+            default:
+                console.log("new message: ", data);
+                break;
+        }
+    
+    };
+    
+    socket.onerror = function(e){
+        console.log("error ",e);
+    };
+    
+    socket.onclose = function(e){
+        console.log("closed ",e);
+    };
+    
+    
+})();
